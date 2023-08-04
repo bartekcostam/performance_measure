b985b8ec328b1bb1b67ce773fbdf95f2f567db85
Bartlomiej Wydmuch <bartlomiej.wydmuch@contractors.roche.com>
2023-08-04 13:16:22 +0200

    fixing bugs and providing option to change loop number

--- gui.py
+++ gui.py
@@ -1,21 +1,44 @@
-from tkinter import Tk, Label, Entry, Button, StringVar
+from tkinter import Tk, Label, Entry, Button, StringVar, IntVar, Checkbutton
 from test import test_loading_speed
+import subprocess
 
 class GUI:
-    def __init__(self):
+    def __init__(self, start_test):
         self.window = Tk()
         self.window.title("Loading Speed Test")
-        self.window.geometry("300x100")
+        self.window.geometry("400x300")
         self.url = StringVar()
-
-
-        Label(self.window, text="URL:").pack()
+        self.xpath = StringVar()
+        
+        self.headless_mode_var = IntVar()
+        self.num_iterations = IntVar(value=5)  # Default value
+        Label(self.window, text="Number of iterations:").pack()
+        Entry(self.window, textvariable=self.num_iterations).pack()
+        Label(self.window, text="XPath of the element to wait for:").pack()
+        Entry(self.window, textvariable=self.xpath).pack()
+        Label(self.window, text="URL: (starts with protocol 'http', 'https')").pack()
         Entry(self.window, textvariable=self.url).pack()
-        Button(self.window, text="Start Test", command=self.start_test).pack()
 
+        # Checkbox for headless mode
+        Checkbutton(self.window, text="Run in headless mode", variable=self.headless_mode_var).pack()
+
+        Button(self.window, text="Start Test", command=start_test, width=20, height=2, bg="lightblue", font=("Helvetica", 12)).pack(pady=5)
+        Button(self.window, text="Show Diagram", command=self.show_diagram, width=20, height=2, bg="lightgreen", font=("Helvetica", 12)).pack(pady=5)
+        
+    def show_diagram(self):
+        subprocess.run(["python", "diagram.py"])
+
+    def run(self):
+        self.window.mainloop()
+
+    def is_headless_mode(self):
+        return self.headless_mode_var.get() == 1
     def start_test(self):
         # Call function from main.py to start the test
         test_loading_speed(self.url.get())
 
     def run(self):
         self.window.mainloop()
+
+    def get_num_iterations(self):
+        return self.num_iterations.get()
--- main.py
+++ main.py
@@ -2,21 +2,62 @@ from gui import GUI
 from prepare_env import setup_environment
 from selenium import webdriver
 import time
+import csv
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
-def test_loading_speed(url):
+def start_test():
+    # Implement your test logic here
+    url = gui.url.get()  # Get the URL from the GUI entry field
+    xpath = gui.xpath.get()  # Get the XPath from the GUI entry field
+    headless_mode = gui.is_headless_mode()
+    print(f"Starting test for URL: {url}")
+    num_iterations = gui.get_num_iterations()
+    results = []
+    for i in range(num_iterations):
+        print(f"Running iteration {i + 1}")
+        loading_time, element_time = test_loading_speed(url, xpath,headless_mode)
+        results.append((i + 1, loading_time, element_time))
+
+    # Save the results to a CSV file
+    with open('results.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Iteration', 'Loading Time (seconds)', 'Element Time (seconds)'])  # Write the header row
+        writer.writerows(results)  # Write the results
+
+    print("Test complete. Results saved to results.csv.")
+
+def test_loading_speed(url, xpath,headless_mode):
     # Set up the environment and get the path to the WebDriver
-    driver_path = setup_environment()
-
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = "./chrome_browser/win/browser/Chrome.exe"
+    chrome_options.add_argument("--incognito")
+    
+    if headless_mode:
+        chrome_options.add_argument("--headless")
     # Use Selenium to test the loading speed
-    driver = webdriver.Chrome(driver_path)
+    driver = webdriver.Chrome("./chrome_browser/win/driver/chromedriver.exe", options=chrome_options)
     start_time = time.time()
-    driver.get(url)
+
+    driver.get(str(url))
+
     end_time = time.time()
     loading_time = end_time - start_time
-    print(f"The loading time for {url} is {loading_time} seconds")
-    driver.quit()
-    # Display the result in the GUI
-    # ...
 
-gui = GUI()
+    # Wait for the element to be available
+    element_start_time = time.time()
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
+    element_end_time = time.time()
+    element_time = element_end_time - element_start_time
+
+    print(f"The loading time for {url} is {loading_time} seconds")
+    print(f"The time for the element at {xpath} to be available is {element_time} seconds")
+    driver.quit()
+
+    return loading_time, element_time
+
+
+gui = GUI(start_test)
 gui.run()
