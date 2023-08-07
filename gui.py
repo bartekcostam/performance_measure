67421a359b9e1c01b73cef0487f83e190f03352b
Bartlomiej Wydmuch <bartlomiej.wydmuch@contractors.roche.com>
2023-08-07 13:13:29 +0200

    adding tabs and other staff

--- gui.py
+++ gui.py
@@ -3,18 +3,27 @@ import settings
 from tkinter import StringVar, IntVar
 from test import test_loading_speed
 import subprocess
+from customtkinter import CTkTabview
 
 class GUI:
     def __init__(self, start_test):
         self.window = ctk.CTk()
         self.window.title("Loading Speed Test")
-        self.window.geometry("600x400")
+        self.window.geometry("600x800")
         self.url = StringVar()
         self.xpath = StringVar()
         self.headless_mode_var = IntVar()
         self.num_iterations = IntVar(value=5)  # Default value
         self.upload_speed = StringVar()
         self.download_speed = StringVar()
+
+        # Create a CTkTabview
+        self.tabview = ctk.CTkTabview(self.window)
+        self.tabview.pack(padx=20, pady=20)
+
+        # Add tabs
+        tab1 = self.tabview.add("Tab 1")
+        tab2 = self.tabview.add("Tab 2")
 
         right_frame = ctk.CTkFrame(self.window)  # Create a frame to hold the right-side widgets
         right_frame.pack(side="right")  # Pack the frame to the right side of the window
@@ -36,14 +45,16 @@ class GUI:
         left_frame = ctk.CTkFrame(self.window)
         left_frame.pack(side="left")
 
-        ctk.CTkLabel(left_frame, text="Internet Speed").pack()
-        ctk.CTkCheckBox(left_frame, text="Option 1").pack()
-        ctk.CTkCheckBox(left_frame, text="Option 2").pack()
-        ctk.CTkCheckBox(left_frame, text="Option 3").pack()
-        ctk.CTkCheckBox(left_frame, text="Option 4").pack()
+        ctk.CTkLabel(left_frame, text=" Internet Speed (Optional) ").pack()
+        ctk.CTkCheckBox(left_frame, text="3G good").pack(padx=5, pady=5)
+        ctk.CTkCheckBox(left_frame, text="3G weak").pack(padx=5, pady=5)
+        ctk.CTkCheckBox(left_frame, text="4G good").pack(padx=5, pady=5)
+        ctk.CTkCheckBox(left_frame, text="4G weak").pack(padx=5, pady=5)
         ctk.CTkLabel(left_frame, text="Upload Speed (Mbps):").pack()
         ctk.CTkEntry(left_frame, textvariable=self.upload_speed).pack()
         ctk.CTkLabel(left_frame, text="Download Speed (Mbps):").pack()
+        ctk.CTkEntry(left_frame, textvariable=self.download_speed).pack()
+        ctk.CTkLabel(left_frame, text="Latancy (ms):").pack()
         ctk.CTkEntry(left_frame, textvariable=self.download_speed).pack()
 
         #Bottom frame
@@ -55,6 +66,8 @@ class GUI:
         self.progressbar = ctk.CTkProgressBar(master=bot_frame, mode="determinate")
         self.progressbar.pack(padx=20, pady=10)
 
+
+   
 
     def set_progressbar(self, value):
         print(value)
