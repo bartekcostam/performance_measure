8c6a81b96236bf8fa573d0bb60108d5bd6ce9e23
filippawlicki <filip.pawlicki77@gmail.com>
2023-08-19 21:36:38 +0200

    add better diagram

--- diagram.py
+++ diagram.py
@@ -1,6 +1,8 @@
 import csv
 import matplotlib.pyplot as plt
 import matplotlib
+from ing_theme_matplotlib import mpl_style
+mpl_style(dark=True)
 matplotlib.use('TkAgg')
 
 class DIAGRAM:
@@ -22,12 +24,24 @@ class DIAGRAM:
         avg_element_time = sum(element_times) / len(element_times)  # Calculate the average element time
 
         # Plot the loading times for each iteration
-        plt.plot(iterations, loading_times, marker='o', label='Loading Time')
-        plt.plot(iterations, element_times, marker='x', label='Element Time')  # Plot the element times
-        plt.axhline(y=avg_loading_time, color='r', linestyle='--', label='Average Loading Time')
-        plt.axhline(y=avg_element_time, color='g', linestyle='--', label='Average Element Time')  # Plot the average element time
-        plt.xlabel('Iteration')
-        plt.ylabel('Time (seconds)')
-        plt.title('Web Page Loading Speed Test')
-        plt.legend()
+        fig, (ax1, ax2) = plt.subplots(2)
+        fig = plt.gcf()
+        fig.canvas.manager.set_window_title('Performance Measure App Diagram')
+        ax1.plot(iterations, loading_times, 'tab:orange' ,marker='o', label='Loading Time')
+        ax1.axhline(y=avg_loading_time, color='r', linestyle='--', label='Average Loading Time')
+        ax1.set_title('Page Loading Time')
+        ax1.set(xlabel='Iteration', ylabel='Time (seconds)')
+        ax2.plot(iterations, element_times, 'tab:blue', marker='o', label='Element Time')
+        ax2.axhline(y=avg_element_time, color='g', linestyle='--', label='Average Element Time')
+        ax2.set_title('Element Loading Time')
+        ax2.set(xlabel='Iteration', ylabel='Time (seconds)')
+
+        ax1.label_outer()
+        ax2.label_outer()
+
+        fig.legend(bbox_to_anchor=(1.015, 0.9), loc='right')
+        fig.set_size_inches(7, 5)
+        fig.tight_layout(pad=2.5)
         plt.show()
+
+
